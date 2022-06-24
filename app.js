483abcb25043c045ac60de158a4726ee996d8630
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-06-23 22:20:25 -0400

    public-directory-added

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const { campgroundSchema, reviewSchema } = require("./schemas.js");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
-const Campground = require("./models/campground");
-const Review = require("./models/review");
+
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {});
@@ -26,16 +23,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
