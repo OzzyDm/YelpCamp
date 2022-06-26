fd1cf23b303cdbb9619534ac79b8a88209241d9a
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-06-26 15:41:26 -0400

    logout-added

--- routes/users.js
+++ routes/users.js
@@ -38,4 +38,11 @@ router.post(
     res.redirect("/campgrounds");
   }
 );
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "Goodbye!");
+  res.redirect("/campgrounds");
+});
+
 module.exports = router;
