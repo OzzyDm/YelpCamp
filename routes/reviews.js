c5e39e70990d553d64ca6c7827ae90d0f406660a
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-06-30 14:56:17 -0400

    reviews-updated

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require("../middleware");
+const { validateReview, isLoggedIn } = require("../middleware");
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
@@ -9,9 +9,11 @@ const Review = require("../models/review");
 router.post(
   "/",
   validateReview,
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
