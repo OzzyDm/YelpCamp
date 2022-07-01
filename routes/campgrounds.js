a0824d662197f859bcd37f88c671e34e7ea32e69
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-07-01 17:14:23 -0400

    cloudinary-basics-added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,13 +4,15 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
   .post(upload.array("image"), (req, res) => {
-    res.send(req.body, req.files);
+    console.log(req.body, req.files);
+    res.send("it worked");
   });
 //.post(
 //isLoggedIn,
