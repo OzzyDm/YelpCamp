c459d15de2018e49fbc25689b06c0f7caf1d9ede
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-07-10 23:44:02 -0400

    add-start-script

--- app.js
+++ app.js
@@ -145,6 +145,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("3000 port");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
