f55073b1fd54bfe1538e97408c6c08995efd70ed
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-07-10 23:37:02 -0400

    ready-to-try-deploying

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const MongoDBStore = require("connect-mongo")(session);
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
@@ -39,9 +40,11 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || "secretsecretsecret";
+
 const store = new MongoDBStore({
   url: dbUrl,
-  secret: "secretsecretsecret",
+  secret,
   touchAfter: 24 * 60 * 60,
 });
 
@@ -52,7 +55,7 @@ store.on("error", function (e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "secretsecretsecret",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
